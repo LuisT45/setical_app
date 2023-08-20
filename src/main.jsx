04a7d6d6564c474973477a4819cc98c310fe9b93
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-20 20:39:44 -0300

    feat: Add nextui to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+//Libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+//Components and Style sheet
 import App from "./App.jsx";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<NextUIProvider>
+			<App />
+		</NextUIProvider>
 	</React.StrictMode>
 );
