614405e2b18c3fd68a3f127a38b4a9e53c2477d5
LuisTorres <lt60900@gmail.com>
2023-08-25 19:26:42 -0300

    feat: Add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { DataDisplay } from "./components/containers/Card";
 import Dashboard from "./components/containers/Dashboard";
 import Layout from "./components/layouts/layout";
 
 function App() {
-	return (
-		<Layout>
-			<Dashboard></Dashboard>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Dashboard>
+        <DataDisplay />
+      </Dashboard>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/containers/Card.jsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Chip,
+  CircularProgress,
+} from "@nextui-org/react";
+
+export const DataDisplay = () => {
+  return (
+    <Card className="w-[240px] h-[240px]">
+      <CardBody className="justify-center items-center p-5">
+        <CircularProgress
+          classNames={{
+            svg: "w-36 h-36 drop-shadow-md",
+            indicator: "stroke-white",
+            track: "stroke-white/10",
+            value: "text-3xl font-semibold text-white",
+          }}
+          value={70}
+          strokeWidth={4}
+          showValueLabel={true}
+        />
+      </CardBody>
+      <CardFooter>
+        <Chip></Chip>
+      </CardFooter>
+    </Card>
+  );
+};
